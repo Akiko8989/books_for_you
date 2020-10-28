7e6f4eaa720641015b1b85bf42672a135e61d209
AkikoMatsui <akk8989162@gmail.com>
2020-10-28 20:20:43 +0900

    ヘッダーの変更

--- app/controllers/trouble_posts_controller.rb
+++ app/controllers/trouble_posts_controller.rb
@@ -7,7 +7,7 @@ class TroublePostsController < ApplicationController
 
   def create
     trouble_post = current_user.trouble_posts.create!(trouble_post_params)
-    redirect_to trouble_posts_path
+    redirect_to root_path
   end
 
   def destroy
