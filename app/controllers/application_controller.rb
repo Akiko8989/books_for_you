50843cbf7d95029e0bf5b2147cf2df94ab8bf59a
AkikoMatsui <akk8989162@gmail.com>
2020-11-09 19:41:05 +0900

    マイページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :username, :password, :password_confirmation, :favorite_genre, :recomended_one, :recomended_one_point, :recomended_two, :recomended_three, :recomended_four, :recomended_five]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 end
